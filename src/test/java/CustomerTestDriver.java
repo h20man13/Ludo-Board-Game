48706d22b0fd920395253d555070ae61578c91dd
root <root@jacob-HP-Spectre-x360-Convertible-13-ac0XX>
2019-03-14 18:27:04 -0400

    This is committing both of the java test class files

---
+++ src/test/java/CustomerTestDriver.java
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+// File:             Customer.java
+// Course:           CSC232, Spring
+// Authors:          Jacob Bauer
+//
+// Acknowledgements: Prof. Howard
+//                   Provided me with the instructions and the description of the project
+//
+// Online sources:  https://moodle3.depauw.edu/mod/assign/view.php?id=203387
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/**                                                                                                                                                                                                                                           
+   The <code>CustomerTestDriver</code> class is used to test the customer class                                                                                                                                                                                
+   @author Jacob Bauer <jacobbauer_2021@depauw.edu>                                                                                                                                                                                           
+*/
+
+public class CustomerTestDriver
+{
+  public static void main(String[] args)
+  {
+    Customer BH = new Customer("Brian","Howard" , ' ' , 53);
+    System.out.println(BH.toString());
+    Customer BC = new Customer(args[0] , args[1], 'A', 52);
+    System.out.println(BC.toString());
+  }
+}
