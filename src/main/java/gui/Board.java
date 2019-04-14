92b1ea7b73d433127c173a75433c1d253554c127
root <root@jacob-HP-Spectre-x360-Convertible-13-ac0XX>
2019-04-14 17:00:13 -0400

    Working Board

--- src/main/java/gui/Board.java
+++ src/main/java/gui/Board.java
@@ -21,7 +21,7 @@ public class Board
    private static SquareBoard[] MainSquares = new SquareBoard[4];
    private static CircleBoard[] StartingLocations = new CircleBoard[16];
    private static TriangleBoard[] endingLocations = new TriangleBoard[4];
-   private static SquareBoard[] MainPath = new SquareBoard[24];
+   private static SquareBoard[] MainPath = new SquareBoard[52];
    private boolean tf = true;
    public Board(int offsetx, int offsety, int size)
    {
@@ -43,8 +43,22 @@ public class Board
          endingLocations[2] = new TriangleBoard(new Coordinates(MainSquares[2].getCoordinates().X() + MainSquares[2].getSize() + MainSquares[2].getSize()/4, MainSquares[2].getCoordinates().Y()), MainSquares[2].getSize()/2, MainSquares[2].getSize()/4, "up", Color.BLUE);
          endingLocations[3] = new TriangleBoard(new Coordinates(MainSquares[2].getCoordinates().X() + MainSquares[2].getSize() + MainSquares[2].getSize()/2,MainSquares[2].getCoordinates().Y() - MainSquares[2].getSize()/4), MainSquares[2].getSize()/2, MainSquares[2].getSize()/4, "left", Color.YELLOW); //creates the triangles
          
-         genSpaces(new Coordinates(MainSquares[0].getCoordinates().X() + MainSquares[0].getSize(), MainSquares[0].getCoordinates().Y()), 6, MainPath, 0, "down", Color.WHITE);
-         genSpaces(new Coordinates(MainSquares[1].getCoordinates().X() - MainSquares[1].getSize()/6, MainSquares[1].getCoordinates().Y()), 2, MainPath, 6, "left", Color.WHITE);  
+         genSpaces(new Coordinates(MainSquares[0].getCoordinates().X() + MainSquares[0].getSize(), MainSquares[0].getCoordinates().Y()), 3, MainPath, 0, "right", Color.WHITE);
+         MainPath[3] = new SquareBoard(new Coordinates(MainPath[2].getCoordinates().X(), MainPath[2].getCoordinates().Y() + MainPath[2].getSize()), MainPath[2].getSize(), Color.GREEN);
+         genSpaces(new Coordinates(MainPath[3].getCoordinates().X(), MainPath[3].getCoordinates().Y() + MainPath[3].getSize()), 4, MainPath, 4, "down", Color.WHITE);
+         genSpaces(new Coordinates(MainSquares[1].getCoordinates().X(), MainSquares[1].getCoordinates().Y() + MainSquares[1].getSize()), 6, MainPath, 8, "right", Color.WHITE);
+         genSpaces(new Coordinates(MainPath[13].getCoordinates().X(), MainPath[13].getCoordinates().Y() + MainPath[13].getSize()), 2, MainPath, 14, "down", Color.WHITE);
+         MainPath[16] = new SquareBoard(new Coordinates(MainPath[15].getCoordinates().X() - MainPath[15].getSize(), MainPath[15].getCoordinates().Y()), MainPath[2].getSize(), Color.YELLOW);
+         genSpaces(new Coordinates(MainPath[16].getCoordinates().X() - MainPath[16].getSize(), MainPath[16].getCoordinates().Y()), 4, MainPath, 17 , "left", Color.WHITE);
+         genSpaces(new Coordinates(MainSquares[3].getCoordinates().X() - MainPath[3].getSize(), MainSquares[3].getCoordinates().Y()), 6, MainPath, 21, "down", Color.WHITE);
+         genSpaces(new Coordinates(MainPath[26].getCoordinates().X() - MainPath[26].getSize(), MainPath[26].getCoordinates().Y()), 2, MainPath, 27, "left", Color.WHITE);
+         MainPath[29] = new SquareBoard(new Coordinates(MainPath[28].getCoordinates().X(), MainPath[28].getCoordinates().Y() - MainPath[28].getSize()), MainPath[28].getSize(), Color.BLUE);
+         genSpaces(new Coordinates(MainPath[29].getCoordinates().X(), MainPath[29].getCoordinates().Y() - MainPath[29].getSize()), 4, MainPath, 30, "up", Color.WHITE);
+         genSpaces(new Coordinates(MainSquares[2].getCoordinates().X() + MainSquares[2].getSize(), MainSquares[2].getCoordinates().Y() - MainPath[33].getSize()), 6, MainPath, 34, "left", Color.WHITE);
+         genSpaces(new Coordinates(MainPath[39].getCoordinates().X(), MainPath[39].getCoordinates().Y() - MainPath[39].getSize()), 2, MainPath, 40, "up", Color.WHITE);
+         MainPath[42] = new SquareBoard(new Coordinates(MainPath[41].getCoordinates().X() + MainPath[41].getSize(), MainPath[41].getCoordinates().Y()), MainPath[41].getSize(), Color.RED);
+         genSpaces(new Coordinates(MainPath[42].getCoordinates().X() + MainPath[42].getSize(), MainPath[42].getCoordinates().Y()), 4, MainPath, 43, "right", Color.WHITE);
+         genSpaces(new Coordinates(MainSquares[1].getCoordinates().X() + MainSquares[1].getSize(), MainSquares[1].getCoordinates().Y() + MainSquares[1].getSize() - MainPath[46].getSize()), 5, MainPath, 47, "up", Color.WHITE);  
       }
       else
       {
